5f83ab0b6f429a1e3acfb2014b95b8d516c9f65e
jaska <farisgithub02@gmail.com>
2026-09-01 23:27:00 +0900

    feat: develop removeCommentByAdmin GraphQL api

--- apps/nestar-api/src/components/comment/comment.service.ts
+++ apps/nestar-api/src/components/comment/comment.service.ts
@@ -105,4 +105,12 @@ export class CommentService {
 
         return result[0];
     }
+
+    /** ADMIN **/
+
+    public async removeCommentByAdmin(commentId: ObjectId): Promise<Comment> {
+        const result = await this.commentModel.findByIdAndDelete(commentId);
+        if (!result) throw new InternalServerErrorException(Message.REMOVE_FAILED);
+        return result;
+    }
 }
